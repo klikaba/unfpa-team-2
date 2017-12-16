80b20046087c387c69f53778cfbf9a418ea90f93
Muhamed Isabegović <m.isabegovic@hotmail.com>
2017-12-16 11:36:21 +0100

    improves style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Welcome!';
-  subtitle = 'powered by: Klika & UNFPA';
+export class AppComponent implements OnInit {
+	elementToShow = { title: "", content: "" };
+
+  illnesses = [
+  {title: "AAAA", content: "xxXX"},
+  {title: "bbbb", content: "baaabbb"},
+  {title: "cccc", content: "cccwwwwc"}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.elementToShow = this.illnesses[0];
+  }
+
+  myEvent(event){
+  for(let element of this.illnesses){
+      if(element.title == event){
+        this.elementToShow = element;
+      }
+    }
+  }
 }
